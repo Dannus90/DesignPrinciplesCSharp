8334864deb4aeccd62d7ee4b78173f744eb94b1a
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-07-17 13:22:00 +0200

    Add InterfaceSegregationPrinciple

--- DesignPrinciplesCSharp/Program.cs
+++ DesignPrinciplesCSharp/Program.cs
@@ -1,7 +1,9 @@
-﻿using DesignPrinciplesCSharp.Solid.LiskovSubstitutionPrinciple;
+﻿using DesignPrinciplesCSharp.Solid.InterfaceSegregationPrinciple;
+using DesignPrinciplesCSharp.Solid.LiskovSubstitutionPrinciple;
 using DesignPrinciplesCSharp.Solid.OpenClosedPrinciple;
 using DesignPrinciplesCSharp.Solid.SingleResponsibilityPrinciple;
 
 SingleResponsibilityPrinciple.Execute();
 OpenClosedPrinciple.Execute();
 LiskovSubstitutionPrinciple.Execute();
+InterfaceSegregationPrinciple.Execute();
---
+++ DesignPrinciplesCSharp/Solid/InterfaceSegregationPrinciple/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,8 @@
+namespace DesignPrinciplesCSharp.Solid.InterfaceSegregationPrinciple;
+
+public class InterfaceSegregationPrinciple
+{
+    public static void Execute()
+    {
+    }
+}
