436e8209edd349606bced8bc438bfd20c886ac8e
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-07-17 13:40:58 +0200

    Add base for dependency inversion principle

---
+++ DesignPrinciplesCSharp/Solid/DependencyInversionPrinciple/DependencyInversionPrinciple.cs
@@ -0,0 +1,9 @@
+namespace DesignPrinciplesCSharp.Solid.DependencyInversionPrinciple;
+
+public static class DependencyInversionPrinciple
+{
+    public static void Execute()
+    {
+        Console.WriteLine("Executing dependency inversion principle");
+    }
+}
