418e768bb21a7312ec793a9e1bb7fb3ff4be9df6
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-07-18 12:52:17 +0200

    Add project for builder pattern

---
+++ Builder/Builder.cs
@@ -0,0 +1,9 @@
+namespace Builder;
+
+public static class Builder
+{
+    public static void RunBuilder()
+    {
+        Console.WriteLine("Running builder");
+    }
+}
--- DesignPrinciplesCSharp/Program.cs
+++ DesignPrinciplesCSharp/Program.cs
@@ -3,9 +3,15 @@ using DesignPrinciplesCSharp.Solid.InterfaceSegregationPrinciple;
 using DesignPrinciplesCSharp.Solid.LiskovSubstitutionPrinciple;
 using DesignPrinciplesCSharp.Solid.OpenClosedPrinciple;
 using DesignPrinciplesCSharp.Solid.SingleResponsibilityPrinciple;
+using static Builder.Builder;
 
+
+// SOLID
 SingleResponsibilityPrinciple.Execute();
 OpenClosedPrinciple.Execute();
 LiskovSubstitutionPrinciple.Execute();
 InterfaceSegregationPrinciple.Execute();
 DependencyInversionPrinciple.Execute();
+
+// BUILDER
+RunBuilder();
