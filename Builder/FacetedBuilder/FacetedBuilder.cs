66486ac2b55d781577849cc2af642f10e1a1a1b4
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-07-23 11:20:20 +0200

    Finish Facade pattern

--- Builder/FacetedBuilder/FacetedBuilder.cs
+++ Builder/FacetedBuilder/FacetedBuilder.cs
@@ -1,15 +1,19 @@
 namespace Builder.FacetedBuilder;
-using static System.Console;
+using static Console;
 
 /// <summary>
 /// More convenient to use.
+/// Side effect is that you can chain events and overwrite them in the same creation.
+/// Facade hides a lot of information behind it. 
 /// </summary>
 public static class FacetedBuilder
 {
     public static void ExecuteFacetedBuilder()
     {
         var personBuilder = new PersonBuilder();
-        var person = personBuilder.Works
+        
+        // Giving the type here lets us do to string correctly based on the person builder setup.
+        Person person = personBuilder.Works
             .WorksAt("FrontEdge IT")
             .WorksAs("Developer")
             .Earns(50000)
--- Builder/FacetedBuilder/PersonBuilder.cs
+++ Builder/FacetedBuilder/PersonBuilder.cs
@@ -5,6 +5,11 @@ public class PersonBuilder // Facade
     // Reference
     protected Person Person = new();
 
-    public PersonJobBuilder Works => new PersonJobBuilder(Person);
-    public PersonAddressBuilder Lives => new PersonAddressBuilder(Person);
+    public PersonJobBuilder Works => new(Person);
+    public PersonAddressBuilder Lives => new(Person);
+
+    public static implicit operator Person(PersonBuilder pb)
+    {
+        return pb.Person;
+    }
 }
