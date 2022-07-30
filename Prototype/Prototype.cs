1ac8a4ff201fcaed73680676d1689e7c24573a57
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-07-30 18:23:57 +0200

    Add comments

--- Prototype/Prototype.cs
+++ Prototype/Prototype.cs
@@ -1,5 +1,11 @@
 ﻿namespace Prototype;
 
+/// <summary>
+/// Complicated objects aren't designed from scratch. They reiterate existing designs.
+/// An exiting (partially or fully constructed) design is a Prototype.
+/// We make a copy (clone) the prototype and customize it (Requires deep copy support).
+/// We make te cloning convenient through a factory. 
+/// </summary>
 public static class Prototype
 {
     public static void RunPrototypes()
