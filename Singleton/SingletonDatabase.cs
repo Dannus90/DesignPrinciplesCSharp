1bd4e48c8f86795a6634f469c77bfa413b30f619
DANIEL Persson <persson.daniel.1990@gmail.com>
2022-08-13 12:06:04 +0200

    Add unit tests

--- Singleton/SingletonDatabase.cs
+++ Singleton/SingletonDatabase.cs
@@ -6,18 +6,23 @@ namespace Singleton;
 public class SingletonDatabase : IDatabase
 {
     private readonly Dictionary<string, int> _capitals;
+    private static int _instanceCount;
+    public static int Count => _instanceCount;
 
     private SingletonDatabase()
     {
         Console.WriteLine("Initializing database");
         var directory = Environment.CurrentDirectory;
         var parentDirectory = Directory.GetParent(directory)?.Parent?.Parent?.FullName;
+        Console.WriteLine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName);
+        Console.WriteLine(Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName ?? throw new InvalidOperationException(), "capitals.txt"));
         _capitals = File.ReadAllLines($"{parentDirectory}/capitals.txt").
             Batch(2).ToDictionary(
                 list => 
                     list.ElementAt(0).ToString(),
                 list => 
                     int.Parse(list.ElementAt(1)));
+        _instanceCount++;
     }
     
     public int GetPopulation(string name)
